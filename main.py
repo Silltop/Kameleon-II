4ae13b420dedc4ee0a42f5243fa931eed0befede
patryk <44586255+Silltop@users.noreply.github.com>
2023-06-11 18:19:51 +0200

    Scaffold update.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import render_template
 from flask_init import app
+import logging_setup
+
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    app.logger.info('hello there')
+    variable = "HELLO THERE"
+    return render_template("index.html", ala=variable)
 
 
 # # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
     app.run()
